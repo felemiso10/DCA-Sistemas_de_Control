97ec4af13fc21ad9e9809884f687a7cdcf671bcc
Rafael Miso <rmg150@gcloud.ua.es>
2021-12-28 06:51:43 +0100

    Bug arreglado por despues de usar bisect

--- P1Plantilla/P1Plantilla/Fuente/aEstrella.py
+++ P1Plantilla/P1Plantilla/Fuente/aEstrella.py
@@ -33,7 +33,7 @@ def adyacentes (casilla):
 
 def exploracion(mapa, casilla, destino, nodoBest, lInterior, lFrontera):
     coste = 0
-    pos = 1 #bug
+    pos = -1 #bug arreglado
     g = 0
     h = 0
     esta = False
